de10bb17bc3f43bef82dbe256fcdafc71b583fa2
nitishKumar-TechM <nitish.kumar65@techmahindra.com>
2024-12-15 18:23:46 +0530

    Player-name editing funtionality added

--- 04-Tic-tac-toe-game/src/components/Player.jsx
+++ 04-Tic-tac-toe-game/src/components/Player.jsx
@@ -1,16 +1,33 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
-  function handleEditClick() {}
+  function handleEditClick() {
+    setIsEditing((prev) => !prev);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  //   let btnCaption = "Edit";
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+    // btnCaption = "Save";
+  }
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
