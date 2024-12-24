ff8c3598d04614a0807921700fcb34c546353e2c
nitishKumar-TechM <nitish.kumar65@techmahindra.com>
2024-12-24 15:58:53 +0530

    Game complete done

--- 04-Tic-tac-toe-game/src/App.jsx
+++ 04-Tic-tac-toe-game/src/App.jsx
@@ -1,16 +1,124 @@
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
+import { useState } from "react";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
+import GameOver from "./components/GameOver";
+
+//Helper function
+function derivedActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+
+  return currentPlayer;
+}
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+function derivedGameBoard(gameTurns){
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  return gameBoard;
+}
+
+function derivedWinner(gameBoard,players){
+  let winner = null;
+
+  for(const combination of WINNING_COMBINATIONS){
+    const firstSquareSymbol =  gameBoard[combination[0].row][combination[0].column]
+    const secondSquareSymbol =   gameBoard[combination[1].row][combination[1].column]
+    const thirdSquareSymbol =   gameBoard[combination[2].row][combination[2].column]
+
+    if(firstSquareSymbol && 
+       firstSquareSymbol === secondSquareSymbol &&
+       firstSquareSymbol === thirdSquareSymbol
+    ){
+      winner = players[firstSquareSymbol];
+    }
+  }
+
+  return winner;
+}
 
 function App() {
+  const [players, setPlayers]= useState({
+    'X' : 'Player 1',
+    'O' : 'Player 2'
+  })
+  const [gameTurns, setGameTurns] = useState([]);
+  // const [hasWinner, setHasWinner] = useState(false);
+  // const [activePlayer, setActivePlayer] = useState("X");
+
+  const activePlayer = derivedActivePlayer(gameTurns);
+
+  const gameBoard = derivedGameBoard(gameTurns);
+
+  const winner = derivedWinner(gameBoard, players); 
+
+  const hasDraw = gameTurns.length === 9 && ! winner;
+
+  function handleSelectSquareNit(rowIndex, colIndex) {
+    // setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+
+    setGameTurns((prevTurns) => {
+      const currentPlayer = derivedActivePlayer(prevTurns);
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
+  }
+
+  function handleRestart() {
+    setGameTurns([]);
+  }
+
+  function handlePlayerNameChange(symbol, newName){
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol]:newName
+      };
+    });
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+            onChangeName={handlePlayerNameChange}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+            onChangeName={handlePlayerNameChange}
+          />
         </ol>
-        GAME BOARD
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} />}
+        <GameBoard onSelectSquare={handleSelectSquareNit} board={gameBoard} />
       </div>
-      LOG
+      <Log turns={gameTurns} />
     </main>
   );
 }
--- 04-Tic-tac-toe-game/src/components/Player.jsx
+++ 04-Tic-tac-toe-game/src/components/Player.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol }) {
+export default function Player({ initialName, symbol, isActive, onChangeName }) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
     setIsEditing((prev) => !prev);
+    
+    if(isEditing){
+      onChangeName(symbol,playerName);
+    }
   }
 
   function handleChange(event) {
@@ -22,7 +26,7 @@ export default function Player({ initialName, symbol }) {
     // btnCaption = "Save";
   }
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
