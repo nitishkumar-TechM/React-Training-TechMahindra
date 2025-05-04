c7b0181737e86f924659209cae2e6fcc3a5e6e76
nitishKumar-TechM <nitish.kumar65@techmahindra.com>
2025-05-04 06:01:56 +0530

    Controlled Components using useState

--- React-codeWithMosh/React-18-for-beginners/form-build-app/src/components/Form.tsx
+++ React-codeWithMosh/React-18-for-beginners/form-build-app/src/components/Form.tsx
@@ -1,16 +1,29 @@
-import { FormEvent, useRef } from "react";
+import { FormEvent, useState } from "react";
+
+// Controlled Components: All input fields have their value prop to maintain
+// their own state, but in this implementation we also have state variable 
+// called 'person', so it is possible that these sources get out of sync.
+// To solve this problem we should make react a single source of truth
+// set the value attribute to person.name and person.age respectively.
+
+// Out of Sync: Sometimes, the value in the input field and the value in
+// the state variable 'person' might not match.
+// This can happen if they are not updated together properly.
+
+// Single Source of Truth: To avoid this mismatch, you should make
+// React the only source of truth. This means the input fields 
+// should always get their value from the state variable, 
+// and any changes to the input fields should update the state variable. 
+// This way, the state variable and the input fields will always be in sync.
 
 function Form() {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = { name: "", age: 0 };
+  const [person, setPerson] = useState({
+    name: "",
+    age: '',
+  });
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    if (nameRef.current !== null) person.name = nameRef.current.value;
-
-    if (ageRef.current != null) person.age = parseInt(ageRef.current.value);
-
     console.log(person);
   };
 
@@ -21,13 +34,29 @@ function Form() {
           <label htmlFor="name" className="form-lebel">
             Name
           </label>
-          <input id="name" ref={nameRef} type="text" className="form-control" />
+          <input
+            id="name"
+            onChange={(event) =>
+              setPerson({ ...person, name: event.target.value })
+            }
+            value={person.name}
+            type="text"
+            className="form-control"
+          />
         </div>
         <div className="mb-3">
           <label htmlFor="age" className="form-label">
             Age
           </label>
-          <input id="age" ref={ageRef} type="number" className="form-control" />
+          <input
+            id="age"
+            onChange={(event) =>
+              setPerson({ ...person, age: parseInt(event.target.value) })
+            }
+            value={person.age}
+            type="number"
+            className="form-control"
+          />
         </div>
         <button className="btn btn-primary" type="submit">
           Submit
